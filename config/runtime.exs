42d68298adaa571fa36ebe001744ba574360a4f1
pathvoid <accounts@pathvoid.com>
2025-07-27 10:30:58 -0400

    fix: changed port and ignoring elixir ls.

--- config/runtime.exs
+++ config/runtime.exs
@@ -31,7 +31,7 @@ if config_env() == :prod do
       """
 
   host = System.get_env("PHX_HOST") || "example.com"
-  port = String.to_integer(System.get_env("PORT") || "4000")
+  port = String.to_integer(System.get_env("PORT") || "4001")
 
   config :nobullfit, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 
