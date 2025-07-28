8b1c730005838411b58bd7e266963bd2281509b5
pathvoid <accounts@pathvoid.com>
2025-07-28 19:50:26 -0400

    fix: removed unused migration file.

--- priv/repo/migrations/20250728233903_add_updated_at_to_maintenance_settings.exs
+++
@@ -1,9 +0,0 @@
-defmodule Nobullfit.Repo.Migrations.AddUpdatedAtToMaintenanceSettings do
-  use Ecto.Migration
-
-  def change do
-    alter table(:maintenance_settings) do
-      add :updated_at, :utc_datetime, null: false, default: fragment("NOW()")
-    end
-  end
-end
